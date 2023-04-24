316bd40f9985fe119a27cf62d03d87ce5fc136b1
loui <howtopackage@gmail.com>
2023-04-24 14:27:14 -0400

    Gallery update

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,13 +5,35 @@ import Screenshot from '../img/temp/gallery_screenshot.png';
 import { galleryData } from './galleryData';
 import Cookies from 'js-cookie';
 
-import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+
+import { Link, useNavigate } from 'react-router-dom';
 import '../styles/Gallery.scss';
 
+import { useMediaQuery } from 'react-responsive';
+
 const Gallery = () => {
+  //Media Query
+  const isBigScreen = useMediaQuery({ 
+    query: '(min-width: 850px)' 
+  })
+  const isAspectRatio = useMediaQuery({
+    query: '(min-aspect-ratio: 8/3)'
+  })
+  const isTallScreen = useMediaQuery({
+    query: '(min-height: 450px)'
+  })
+
+  const isSizingAspectRatio = useMediaQuery({
+    query: '(max-aspect-ratio: 16/9)'
+  })
+
+
   const navigate = useNavigate();
   const [openMenu, setOpenMenu] = useState(false);
   console.log(galleryData[0]);
+
 
   return (
     <div className="gallery-container">
@@ -22,44 +44,56 @@ const Gallery = () => {
       </div>
       <div className="white-background">
         <div className="border1">
-          <div className="border2">
-            <h1 className="title">Flavour Profiles</h1>
+
+            <h1 className="title">Flavour - Profiles</h1>
             <div className="grid-container">
               <div className="chess">
                 <div className="image">
                   {Cookies.get("chess") ? 
-                    <img src={galleryData[0].characters.chess.visited} alt="Queen Knowah"></img> :
-                    <img src={galleryData[0].characters.chess.locked} alt="Locked Queen Knowah"></img>
+                    <Link to="/chess"><img className="visited" src={galleryData[0].characters.chess.visited} alt="Queen Knowah"></img></Link> :
+                    <>
+                      <img src={galleryData[0].characters.chess.locked} alt="Locked Queen Knowah"></img>
+                      <FontAwesomeIcon className="icon" icon={faLock} />
+                    </>
                   }
-                </div>
-                <div className="name">{galleryData[0].characters.chess.name}</div>
+                </div><br/>
+                <div className="name">{galleryData[0].characters.chess.name}</div><br/>
                 <div className="description">{galleryData[0].characters.chess.description}</div>
               </div>
               <div className="wafer">
                 <div className="image">
                   {Cookies.get("wafer") ? 
-                    <img src={galleryData[0].characters.wafer.visited} alt="Flaky Wafer Jake"></img> :
-                    <img src={galleryData[0].characters.wafer.locked} alt="Locked Flaky Wafer Jake"></img>
+                    <img className="visited" src={galleryData[0].characters.wafer.visited} alt="Flaky Wafer Jake"></img> :
+                    <>
+                      <img src={galleryData[0].characters.wafer.locked} alt="Locked Flaky Wafer Jake"></img>
+                      <FontAwesomeIcon className="icon" icon={faLock} />
+                    </>
                   }
-                </div>
-                <div className="name">{galleryData[0].characters.wafer.name}</div>
+                </div><br/>
+                <div className="name">{galleryData[0].characters.wafer.name}</div><br/>
                 <div className="description">{galleryData[0].characters.wafer.description}</div>
               </div>
               <div className="financier">
                 <div className="image">
                   {Cookies.get("financier") ? 
-                    <img src={galleryData[0].characters.financier.visited} alt="Monsieur Financier"></img> :
-                    <img src={galleryData[0].characters.financier.locked} alt="Locked Monsieur Financier"></img>
+                    <img className="visited" src={galleryData[0].characters.financier.visited} alt="Monsieur Financier"></img> :
+                    <>
+                      <img src={galleryData[0].characters.financier.locked} alt="Locked Monsieur Financier"></img>
+                      <FontAwesomeIcon className="icon" icon={faLock} />
+                    </>
                   }
-                </div>
-                <div className="name">{galleryData[0].characters.financier.name}</div>
+                </div><br/>
+                <div className="name">{galleryData[0].characters.financier.name}</div><br/>
                 <div className="description">{galleryData[0].characters.financier.description}</div>
               </div>
               <div className="macaron">
                 <div className="image">
                   {Cookies.get("macaron") ? 
-                    <img src={galleryData[0].characters.macaron.visited} alt="Madame Macaron"></img> :
-                    <img src={galleryData[0].characters.macaron.locked} alt="Locked Madame Macaron"></img>
+                    <img className="visited" src={galleryData[0].characters.macaron.visited} alt="Madame Macaron"></img> :
+                    <>
+                      <img src={galleryData[0].characters.macaron.locked} alt="Locked Madame Macaron"></img>
+                      <FontAwesomeIcon className="icon" icon={faLock} />
+                    </>
                   }
                 </div>
                 <div className="name">{galleryData[0].characters.macaron.name}</div>
@@ -68,72 +102,91 @@ const Gallery = () => {
               <div className="soft">
                 <div className="image">
                   {Cookies.get("softcookie") ? 
-                    <img src={galleryData[0].characters.soft.visited} alt="Soft Cookie"></img> :
-                    <img src={galleryData[0].characters.soft.locked} alt="Locked Soft Cookie"></img>
+                    <img className="visited" src={galleryData[0].characters.soft.visited} alt="Soft Cookie"></img> :
+                    <>
+                      <img src={galleryData[0].characters.soft.locked} alt="Locked Soft Cookie"></img>
+                      <FontAwesomeIcon className="icon" icon={faLock} />
+                    </>
                   }
-                </div>
-                <div className="name">{galleryData[0].characters.soft.name}</div>
+                </div><br/>
+                <div className="name">{galleryData[0].characters.soft.name}</div><br/>
                 <div className="description">{galleryData[0].characters.soft.description}</div>
               </div>
               <div className="linzer">
                 <div className="image">
                   {Cookies.get("linzer") ? 
-                    <img src={galleryData[0].characters.linzer.visited} alt="Lying Linzer Lindsay"></img> :
-                    <img src={galleryData[0].characters.linzer.locked} alt="Locked Lying Linzer Lindsay"></img>
+                    <img className="visited" src={galleryData[0].characters.linzer.visited} alt="Lying Linzer Lindsay"></img> :
+                    <>
+                      <img src={galleryData[0].characters.linzer.locked} alt="Locked Lying Linzer Lindsay"></img>
+                      <FontAwesomeIcon className="icon" icon={faLock} />
+                    </>
                   }
-                </div>
-                <div className="name">{galleryData[0].characters.linzer.name}</div>
+                </div><br/>
+                <div className="name">{galleryData[0].characters.linzer.name}</div><br/>
                 <div className="description">{galleryData[0].characters.linzer.description}</div>
               </div>
               <div className="brownie">
                 <div className="image">
                   {Cookies.get("brownie") ? 
-                    <img src={galleryData[0].characters.brownie.visited} alt="Little Debbie Downer Brownie"></img> :
-                    <img src={galleryData[0].characters.brownie.locked} alt="Locked Little Debbie Downer Brownie"></img>
+                    <img className="visited" src={galleryData[0].characters.brownie.visited} alt="Little Debbie Downer Brownie"></img> :
+                    <>
+                      <img src={galleryData[0].characters.brownie.locked} alt="Locked Little Debbie Downer Brownie"></img>
+                      <FontAwesomeIcon className="icon" icon={faLock} />
+                    </>
+                  
                   }
-                </div>
-                <div className="name">{galleryData[0].characters.brownie.name}</div>
+                </div><br/>
+                <div className="name">{galleryData[0].characters.brownie.name}</div><br/>
                 <div className="description">{galleryData[0].characters.brownie.description}</div>
               </div>
               <div className="madeleine">
                 <div className="image">
                   {Cookies.get("madeleine") ? 
-                    <img src={galleryData[0].characters.madeleine.visited} alt="Mad Mad Babu Madeleine"></img> :
-                    <img src={galleryData[0].characters.madeleine.locked} alt="Locked Mad Mad Baby Madeleine"></img>
+                    <img className="visited" src={galleryData[0].characters.madeleine.visited} alt="Mad Mad Babu Madeleine"></img> :
+                    <>
+                      <img src={galleryData[0].characters.madeleine.locked} alt="Locked Mad Mad Baby Madeleine"></img>
+                      <FontAwesomeIcon className="icon" icon={faLock} />
+                    </>
                   }
-                </div>
-                <div className="name">{galleryData[0].characters.madeleine.name}</div>
+                </div><br/>
+                <div className="name">{galleryData[0].characters.madeleine.name}</div><br/>
                 <div className="description">{galleryData[0].characters.madeleine.description}</div>
               </div>
               <div className="fortune">
                 <div className="image">
                   {Cookies.get("fortune") ? 
-                    <img src={galleryData[0].characters.fortune.visited} alt="Mis-Fortune"></img> :
-                    <img src={galleryData[0].characters.fortune.locked} alt="Locked Mis-Fortune"></img>
+                    <img className="visited" src={galleryData[0].characters.fortune.visited} alt="Mis-Fortune"></img> :
+                    <>
+                      <img src={galleryData[0].characters.fortune.locked} alt="Locked Mis-Fortune"></img>
+                      <FontAwesomeIcon className="icon" icon={faLock} />
+                    </>
                   }
-                </div>
-                <div className="name">{galleryData[0].characters.fortune.name}</div>
+                </div><br/>
+                <div className="name">{galleryData[0].characters.fortune.name}</div><br/>
                 <div className="description">{galleryData[0].characters.fortune.description}</div>
               </div>
               <div className="mystery1">
                 <div className="image">
-                  <div className="temp-image"></div>
-                </div>
-                <div className="name">? ? ?</div>
+                  <div className="temp-square"></div>
+                </div><br/>
+                <div className="name">? ? ?</div><br/>
                 <div className="description"></div>
               </div>
               <div className="mystery2">
-                <div className="image"></div>
-                <div className="name">? ? ?</div>
+                <div className="image">
+                  <div className="temp-circle"></div>
+                </div><br/>
+                <div className="name">? ? ?</div><br/>
                 <div className="description"></div>
               </div>
               <div className="mystery3">
-                <div className="image"></div>
-                <div className="name">? ? ?</div>
+                <div className="image">
+                  <div className="temp-triangle"></div>
+                </div><br/>
+                <div className="name">? ? ?</div><br/>
                 <div className="description"></div>
               </div>
             </div>
-          </div>
         </div>
       </div>
     </div>
--- src/components/galleryData.js
+++ src/components/galleryData.js
@@ -5,55 +5,55 @@ export const galleryData = [
         "diamond": require('../img/Gallery/Diamond_Doily.png'),
         "characters": {
             "chess": {
-                "locked": "placeholder",
+                "locked": require('../img/Gallery/icons/Grey_Chess.png'),
                 "visited": require('../img/Chess/Chess.png'),
                 "name": "QuEEn knowAh",
                 "description": `Dark Chocolate, White Chocolate,\nCharcoal, & Vanilla Bean`
             },
             "wafer": {
-                "locked": "placeholder",
+                "locked": require('../img/Gallery/icons/Grey_Wafer.png'),
                 "visited": "placeholder",
                 "name": "FLAkEY waFEr JAkE",
                 "description": `Dry, Crunchy, Bland,\nCoconut Flakes w/Taro Cream`
             },
             "financier": {
-                "locked": "placeholder",
+                "locked": require('../img/Gallery/icons/Grey_Financier.png'),
                 "visited": require('../img/Financier/Financier.png'),
                 "name": "moNsiEUr fiNANciEr",
                 "description": `Amedine, Gold Nougats\nButter Bars, & Cheap Sugar`
             },
             "macaron": {
-                "locked": "placeholder",
+                "locked": require('../img/Gallery/icons/Grey_Macaron1.png'),
                 "visited": require('../img/Macaron/Macaron.png'),
                 "name": "mADamE mAcarOn",
                 "description": `Dried Rose, Pow(d)ered Sugar,\nPreserved Lychee,`
             },
             "soft": {
-                "locked": "placeholder",
+                "locked": require('../img/Gallery/icons/Grey_SoftCookie.png'),
                 "visited": "placeholder",
                 "name": "SoFT cookIE(s)",
                 "description": `Soft, Warm, & 7 Senses\n(Tasteful, Fragrant, Textural, Hearty, Visual, Harmonious, & Emotional)`
             },
             "linzer": {
-                "locked": "placeholder",
+                "locked": require('../img/Gallery/icons/Grey_Linzer.png'),
                 "visited": require('../img/Linzer/Linzer.png'),
                 "name": "Lying Linzer Lindsay",
                 "description": `Sour, Tart, Li(m)e, Blood D'orange,\n& Ref Food Colouring`
             },
             "brownie": {
-                "locked": "placeholder",
+                "locked": require('../img/Gallery/icons/Grey_Brownie.png'),
                 "visited": require('../img/Brownie/Brownie.png'),
                 "name": "liTTlE dEBBiE downER BRowniE",
                 "description": `Sea Salt Soaked, Chocolate Tres Leches de Agua, w/ Con.fetti Tears`,
             },
             "madeleine": {
-                "locked": "placeholder",
+                "locked": require('../img/Gallery/icons/Grey_Madeleine.png'),
                 "visited": require('../img/Madeleine/Madeleine.png'),
                 "name": "mad mad baby madeleine",
                 "description": `Red Chili Pepper, Pink Peppercorn,\nStar Anise, & Lemon`
             },
             "fortune": {
-                "locked": "placeholder",
+                "locked": require('../img/Gallery/icons/Grey_Fortune.png'),
                 "visited": require('../img/Fortune/MisFortune.png'),
                 "name": "mis . FOrTUnE",
                 "description": `Rotten Custard, Century Egg,\n& Expired Flour`
