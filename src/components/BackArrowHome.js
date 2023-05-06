cb215250882bcd46a9a514d3b750a6343dc76a4b
loui <howtopackage@gmail.com>
2023-05-05 21:45:09 -0400

    Update BackArrowHome.js

--- src/components/BackArrowHome.js
+++ src/components/BackArrowHome.js
@@ -1,24 +1,23 @@
-import { useState } from 'react';
 import { useLocation, Link, useNavigate } from 'react-router-dom';
 
 const BackArrowHome = () => {
-    const location = useLocation();
-    const section = location.state;
+  const location = useLocation();
+  const { section } = location.state;
+  console.log(section);
+  /*if (location.state) {
+    section = location.state;
     console.log(section);
-    /*if (location.state) {
-      section = location.state;
-      console.log(section);
-    }*/
-    const navigate = useNavigate();
+  }*/
+  const navigate = useNavigate();
 
-  return (
-    <>
-      {(location.state !== "" && location.state !== undefined) ? 
-        <Link className="backarrow" to={`/home#${section}`}></Link> :
-        <div className="backarrow" onClick={() => navigate(-1)}></div>
-    }
-    </>
-    )
+return (
+  <>
+    {(location.state !== "" && location.state !== undefined) ? 
+      <Link className="backarrow" to={`/home#${section}`}></Link> :
+      <div className="backarrow" onClick={() => navigate(-1)}></div>
+  }
+  </>
+  )
 }
 
 export default BackArrowHome
