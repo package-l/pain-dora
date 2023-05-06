66036433c465204f09d1b540db9a5e47befa336e
loui <howtopackage@gmail.com>
2023-05-05 21:42:47 -0400

    Update BackArrowHome navigation issues

--- src/components/BackArrowHome.js
+++ src/components/BackArrowHome.js
@@ -1,18 +1,19 @@
 import { useState } from 'react';
 import { useLocation, Link, useNavigate } from 'react-router-dom';
 
-const BackArrow = () => {
-    const location = useLocation()
-    const [section, setSection] = useState("");
-    if (location.state) {
-      setSection(location.state);
+const BackArrowHome = () => {
+    const location = useLocation();
+    const section = location.state;
+    console.log(section);
+    /*if (location.state) {
+      section = location.state;
       console.log(section);
-    }
+    }*/
     const navigate = useNavigate();
 
   return (
     <>
-      {(section !== "" || section !== null) ? 
+      {(location.state !== "" && location.state !== undefined) ? 
         <Link className="backarrow" to={`/home#${section}`}></Link> :
         <div className="backarrow" onClick={() => navigate(-1)}></div>
     }
@@ -20,4 +21,4 @@ const BackArrow = () => {
     )
 }
 
-export default BackArrow
+export default BackArrowHome
